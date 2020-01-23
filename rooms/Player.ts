79ee3fd965291598dd68c1f5110eacaa97d38c65
MikalDev <keg@kindeyegames.com>
2020-01-23 00:10:36 -0800

    remove colyseus example

--- rooms/Player.ts
+++
@@ -1,8 +0,0 @@
-import { Entity } from "./Entity";
-import { DEFAULT_PLAYER_RADIUS } from "./State";
-
-export class Player extends Entity {
-    constructor(x: number, y: number) {
-        super(x, y, DEFAULT_PLAYER_RADIUS);
-    }
-}
